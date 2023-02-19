65f0233fbc03a514f745f7ce94110a4c85cd3336
cksajil <sajilck@gmail.com>
2023-02-19 20:02:58 +0530

    Clean setup.py using Flake8  On branch sajil-dev  Your branch is up to date with 'origin/sajil-dev'.  Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'MAGAN',
-    version = '0.1',
-    url = 'https://github.com/KrishnaswamyLab/MAGAN',
-    author = 'Matt Amodio',
-    description = '',
-    packages = find_packages()
+    name='MAGAN',
+    version='0.1',
+    url='https://github.com/KrishnaswamyLab/MAGAN',
+    author='Matt Amodio',
+    description='',
+    packages=find_packages()
 )
