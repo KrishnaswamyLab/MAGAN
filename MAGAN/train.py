c11f9a79eff2e425bc24eeed05423529bca2ee9f
cksajil <sajilck@gmail.com>
2023-02-19 20:26:07 +0530

    Clean train.py styling using Flake8 Reduced lengthy lines to shorter ones  On branch sajil-dev  Changes to be committed: 	modified:   MAGAN/train.py

--- MAGAN/train.py
+++ MAGAN/train.py
@@ -1,35 +1,59 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib
+import matplotlib.pyplot as plt
+from numpy.random import normal
+
+from loader import Loader
 from utils import now
 from model import MAGAN
-from loader import Loader
-import matplotlib.pyplot as plt
-import matplotlib.cm
-plt.ion(); fig = plt.figure()
+
+plt.ion()
+fig = plt.figure()
 
 
 def get_data(n_batches=2, n_pts_per_cluster=5000):
     """Return the artificial data."""
-    make = lambda x,y,s: np.concatenate([np.random.normal(x,s, (n_pts_per_cluster, 1)), np.random.normal(y,s, (n_pts_per_cluster, 1))], axis=1)
+
+    make = lambda x, y, s: np.concatenate(
+        [normal(x, s, (n_pts_per_cluster, 1)),
+         normal(y, s, (n_pts_per_cluster, 1))],
+        axis=1)
+
     # batch 1
-    xb1 = np.concatenate([make(-1.3, 2.2, .1), make(.1, 1.8, .1), make(.8, 2, .1)], axis=0)
-    labels1 = np.concatenate([0 * np.ones(n_pts_per_cluster), 1 * np.ones(n_pts_per_cluster), 2 * np.ones(n_pts_per_cluster)], axis=0)
+    xb1 = np.concatenate([make(-1.3, 2.2, .1),
+                          make(.1, 1.8, .1),
+                          make(.8, 2, .1)], axis=0)
+
+    labels1 = np.concatenate([0 * np.ones(n_pts_per_cluster),
+                              1 * np.ones(n_pts_per_cluster),
+                              2 * np.ones(n_pts_per_cluster)], axis=0)
 
     # batch 2
-    xb2 = np.concatenate([make(-.9, -2, .1), make(0, -2.3, .1), make(1.5, -1.5, .1)], axis=0)
-    labels2 = np.concatenate([0 * np.ones(n_pts_per_cluster), 1 * np.ones(n_pts_per_cluster), 2 * np.ones(n_pts_per_cluster)], axis=0)
+    xb2 = np.concatenate([make(-.9, -2, .1),
+                          make(0, -2.3, .1),
+                          make(1.5, -1.5, .1)], axis=0)
+
+    labels2 = np.concatenate([0 * np.ones(n_pts_per_cluster),
+                              1 * np.ones(n_pts_per_cluster),
+                              2 * np.ones(n_pts_per_cluster)], axis=0)
 
     return xb1, xb2, labels1, labels2
+
 
 def correspondence_loss(b1, b2):
     """
     The correspondence loss.
 
-    :param b1: a tensor representing the object in the graph of the current minibatch from domain one
-    :param b2: a tensor representing the object in the graph of the current minibatch from domain two
+    :param b1: a tensor representing the object in the graph
+               of the current minibatch from domain one
+
+    :param b2: a tensor representing the object in the graph
+               of the current minibatch from domain two
+
     :returns a scalar tensor of the correspondence loss
     """
+
     domain1cols = [0]
     domain2cols = [0]
     loss = tf.constant(0.)
@@ -37,6 +61,7 @@ def correspondence_loss(b1, b2):
         loss += tf.reduce_mean((b1[:, c1] - b2[:, c2])**2)
 
     return loss
+
 
 # Load the data
 xb1, xb2, labels1, labels2 = get_data()
@@ -48,11 +73,14 @@ loadb2 = Loader(xb2, labels=labels2, shuffle=True)
 batch_size = 100
 
 # Build the tf graph
-magan = MAGAN(dim_b1=xb1.shape[1], dim_b2=xb2.shape[1], correspondence_loss=correspondence_loss)
+magan = MAGAN(dim_b1=xb1.shape[1],
+              dim_b2=xb2.shape[1],
+              correspondence_loss=correspondence_loss)
 
 # Train
 for i in range(1, 3000):
-    if i % 100 == 0: print("Iter {} ({})".format(i, now()))
+    if i % 100 == 0:
+        print("Iter {} ({})".format(i, now()))
     xb1_, labels1_ = loadb1.next_batch(batch_size)
     xb2_, labels2_ = loadb2.next_batch(batch_size)
 
@@ -66,7 +94,6 @@ for i in range(1, 3000):
         lstring = magan.get_loss(xb1_, xb2_)
         print("{} {}".format(magan.get_loss_names(), lstring))
 
-
         xb1 = magan.get_layer(xb1_, xb2_, 'xb1')
         xb2 = magan.get_layer(xb1_, xb2_, 'xb2')
         Gb1 = magan.get_layer(xb1_, xb2_, 'Gb1')
@@ -76,27 +103,66 @@ for i in range(1, 3000):
         axes = fig.subplots(2, 2, sharex=True, sharey=True)
         axes[0, 0].set_title('Original')
         axes[0, 1].set_title('Generated')
-        axes[0, 0].scatter(0, 0, s=45, c='b', label='Batch 1'); axes[0, 0].scatter(0,0, s=100, c='w'); axes[0, 0].legend(handletextpad=.1, borderpad=.5, loc='center left', bbox_to_anchor=[.02, .5]);
-        axes[0, 1].scatter(0, 0, s=45, c='r', label='Batch 2'); axes[0, 1].scatter(0,0, s=100, c='w'); axes[0, 1].legend(handletextpad=.1, borderpad=.5, loc='center left', bbox_to_anchor=[.02, .5]);
-        axes[1, 0].scatter(0, 0, s=45, c='r', label='Batch 2'); axes[1, 0].scatter(0,0, s=100, c='w'); axes[1, 0].legend(handletextpad=.1, borderpad=.5, loc='center left', bbox_to_anchor=[.02, .5]);
-        axes[1, 1].scatter(0, 0, s=45, c='b', label='Batch 1'); axes[1, 1].scatter(0,0, s=100, c='w'); axes[1, 1].legend(handletextpad=.1, borderpad=.5, loc='center left', bbox_to_anchor=[.02, .5]);
+        axes[0, 0].scatter(0, 0, s=45, c='b', label='Batch 1')
+        axes[0, 0].scatter(0, 0, s=100, c='w')
+        axes[0, 0].legend(handletextpad=.1,
+                          borderpad=.5,
+                          loc='center left',
+                          bbox_to_anchor=[.02, .5])
+
+        axes[0, 1].scatter(0, 0, s=45, c='r', label='Batch 2')
+        axes[0, 1].scatter(0, 0, s=100, c='w')
+        axes[0, 1].legend(handletextpad=.1,
+                          borderpad=.5,
+                          loc='center left',
+                          bbox_to_anchor=[.02, .5])
+
+        axes[1, 0].scatter(0, 0, s=45, c='r', label='Batch 2')
+        axes[1, 0].scatter(0, 0, s=100, c='w')
+
+        axes[1, 0].legend(handletextpad=.1,
+                          borderpad=.5,
+                          loc='center left',
+                          bbox_to_anchor=[.02, .5])
+
+        axes[1, 1].scatter(0, 0, s=45, c='b', label='Batch 1')
+        axes[1, 1].scatter(0, 0, s=100, c='w')
+        axes[1, 1].legend(handletextpad=.1,
+                          borderpad=.5,
+                          loc='center left',
+                          bbox_to_anchor=[.02, .5])
 
         for lab, marker in zip([0, 1, 2], ['x', 'D', '.']):
-            axes[0, 0].scatter(xb1[labels1_ == lab, 0], xb1[labels1_ == lab, 1], s=45, alpha=.5, cmap=matplotlib.cm.jet, c='b', marker=marker)
-            axes[0, 1].scatter(Gb2[labels1_ == lab, 0], Gb2[labels1_ == lab, 1], s=45, alpha=.5, cmap=matplotlib.cm.jet, c='r', marker=marker)
+            axes[0, 0].scatter(xb1[labels1_ == lab, 0],
+                               xb1[labels1_ == lab, 1],
+                               s=45,
+                               alpha=.5,
+                               cmap=matplotlib.cm.jet,
+                               c='b',
+                               marker=marker)
+
+            axes[0, 1].scatter(Gb2[labels1_ == lab, 0],
+                               Gb2[labels1_ == lab, 1],
+                               s=45,
+                               alpha=.5,
+                               cmap=matplotlib.cm.jet,
+                               c='r',
+                               marker=marker)
+
         for lab, marker in zip([0, 1, 2], ['x', 'D', '.']):
-            axes[1, 0].scatter(xb2[labels2_ == lab, 0], xb2[labels2_ == lab, 1], s=45, alpha=.5, cmap=matplotlib.cm.jet, c='r', marker=marker)
-            axes[1, 1].scatter(Gb1[labels2_ == lab, 0], Gb1[labels2_ == lab, 1], s=45, alpha=.5, cmap=matplotlib.cm.jet, c='b', marker=marker)
+            axes[1, 0].scatter(xb2[labels2_ == lab, 0],
+                               xb2[labels2_ == lab, 1],
+                               s=45,
+                               alpha=.5,
+                               cmap=matplotlib.cm.jet,
+                               c='r',
+                               marker=marker)        
+            axes[1, 1].scatter(Gb1[labels2_ == lab, 0],
+                               Gb1[labels2_ == lab, 1],
+                               s=45,
+                               alpha=.5,
+                               cmap=matplotlib.cm.jet,
+                               c='b',
+                               marker=marker)
         fig.canvas.draw()
         plt.pause(1)
-
-
-
-
-
-
-
-
-
-
-
